d3ffd53cab2436f8f67ed5ba7212618e09f5e139
Jasper Moelker <j.b.moelker@activeids.nl>
2017-08-12 00:04:58 +0200

    Update cookie-httponly helptext with better info
    and 
    * always PascalCase HttpOnly
    * add failure description
    * change link to markdown format

--- audits/cookie-httponly.js
+++ audits/cookie-httponly.js
@@ -8,8 +8,10 @@ class CookieHttpOnlyAudit extends Audit {
     return {
       category: 'PageSecurity',
       name: 'cookie-httpOnly-audit',
-      description: 'Cookies are httpOnly',
-      helpText: 'For more information visit https://www.owasp.org/index.php/HttpOnly',
+      description: 'Cookies are HttpOnly',
+      failureDescription: 'Cookies are not HttpOnly',
+      helpText: 'Using the HttpOnly flag when generating a cookie helps mitigate the risk of client side script accessing the protected cookie. ' +
+          '[Learn more](https://www.owasp.org/index.php/HttpOnly)',
 
       // The name of the custom gatherer class that provides input to this audit.
       requiredArtifacts: ['RequestHeaders']
