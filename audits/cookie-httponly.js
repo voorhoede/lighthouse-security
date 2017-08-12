1c668043ff8182b816495ee15a33a02729ad3003
Jasper Moelker <j.b.moelker@activeids.nl>
2017-08-12 13:41:47 +0200

    Remove boilerplate code from cookie-httponly

--- audits/cookie-httponly.js
+++ audits/cookie-httponly.js
@@ -1,8 +1,5 @@
 const Audit = require('lighthouse').Audit;
 
-const MAX_SEARCHABLE_TIME = 4000;
-
-
 class CookieHttpOnlyAudit extends Audit {
   static get meta() {
     return {
@@ -12,8 +9,6 @@ class CookieHttpOnlyAudit extends Audit {
       failureDescription: 'Cookies are not HttpOnly',
       helpText: 'Using the HttpOnly flag when generating a cookie helps mitigate the risk of client side script accessing the protected cookie. ' +
           '[Learn more](https://www.owasp.org/index.php/HttpOnly)',
-
-      // The name of the custom gatherer class that provides input to this audit.
       requiredArtifacts: ['RequestHeaders']
     };
   }
@@ -21,7 +16,6 @@ class CookieHttpOnlyAudit extends Audit {
   static audit(artifacts) {
     const headers = artifacts.RequestHeaders
     const setCookieHeader = headers['set-cookie']
-
     const httpOnly = /HttpOnly/.test(setCookieHeader) || !setCookieHeader
 
     return {
@@ -31,4 +25,4 @@ class CookieHttpOnlyAudit extends Audit {
   }
 }
 
-module.exports = CookieHttpOnlyAudit
+module.exports = CookieHttpOnlyAudit;
