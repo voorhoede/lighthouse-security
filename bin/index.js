ea2b255a82f0c3de76c08895e90a941033b83924
Jasper Moelker <j.b.moelker@activeids.nl>
2017-08-26 15:45:40 +0200

    Chore: use lighthouse-logger for logging

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const runLighthouse = require('../index');
+const log = require('lighthouse-logger');
 const {getFlags} = require('lighthouse/lighthouse-cli/cli-flags');
 const {saveResults} = require('lighthouse/lighthouse-cli/run');
 
@@ -10,5 +11,4 @@ const url = flags._[0];
 
 runLighthouse(url, flags)
   .then(results => saveResults(results, {}, flags))
-  // eslint-disable-next-line no-console
-  .then(() => console.log('Report has been saved successfully!'));
+  .then(() => log.log('CLI', 'Report has been saved successfully!'));
